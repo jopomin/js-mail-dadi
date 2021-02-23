ab64bc526d2971cb68a0ea245b423f1697cc1f42
Giovanni <giovannipor23@gmail.com>
2021-02-23 15:11:32 +0100

    Struttura Iniziale e logica (solo commentata)

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* chiedere email (input mail) e controllare che appartenga (if) ad una lista (array) 
+
+stampare messaggio di conferma/negazione accesso (alert)
+
+generare numero da 1 a 6 (math) per giocatore (var1) e cpu (var2)
+
+confrontare i due valori (if) e decretare il maggiore */
+
