1b8d790cd471aa3ae6c700550b4761ad80f92b49
Giovanni <giovannipor23@gmail.com>
2021-02-23 18:05:32 +0100

    Solo logica (con Input email)

--- js/main.js
+++ js/main.js
@@ -9,29 +9,39 @@ confrontare i due valori (if) e decretare il maggiore */
 var listaMail = ["fabiobianchi@gmail.com", "mariorossi@gmail.com", "simonaverdi@gmail.com", "carlabruni@gmail.com"];
 console.log(listaMail);
 
-var email = prompt('inserisci la tua mail');
-console.log(email);
+var invia = document.getElementById("invia");
+invia.addEventListener('click',
+    function() {
+        var email = document.getElementById("email").value;
+        console.log(email);
+        if (listaMail.includes(email)) {
+            alert ('Benvenuto, preparati a lanciare i dadi');
+            var player = Math.floor(Math.random()*5+1);
+            console.log(player);
+            var cpu = Math.floor(Math.random()*5+1);
+            console.log(cpu);
+        
+            if (player > cpu) {
+                console.log("Hai vinto");
+            }
+            else if (player == cpu) {
+                console.log("Pareggio");
+            }
+            else {
+                console.log("Hai perso");
+            }
+            
+            } else {
+                alert ('Accesso non autorizzato');
+            }
+    }
 
-if (listaMail.includes(email)) {
-    alert ('Benvenuto, preparati a lanciare i dadi');
-    var player = Math.floor(Math.random()*5+1);
-    console.log(player);
-    var cpu = Math.floor(Math.random()*5+1);
-    console.log(cpu);
+)
 
-    if (player > cpu) {
-        console.log("Hai vinto");
-    }
-    else if (player == cpu) {
-        console.log("Pareggio");
-    }
-    else {
-        console.log("Hai perso");
-    }
-    
-    } else {
-        alert ('Accesso non autorizzato');
-    }
+
+/* var email = prompt('inserisci la tua mail');
+console.log(email); */
+
     
 
 /* invia = document.getElementById("invia");
@@ -41,6 +51,6 @@ invia.addEventListener ('click',
         console.log(email);
     }
 
-) */
+)
 
 /* if listaMail.includes() */
